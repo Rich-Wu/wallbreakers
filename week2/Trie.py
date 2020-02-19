90e975677d5505466c8477a003e2c5ae8ff39409
Richard <richie1988@gmail.com>
2020-02-19 14:38:12 -0500

    finished Trie

--- week2/Trie.py
+++ week2/Trie.py
@@ -1,47 +1,62 @@
+class TrieNode:
+    def __init__(self):
+        self.letters = [None] * 26
+        self.isEnd = False
+        
+
 class Trie:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.isEnd = False
-        self.letters = [None for _ in range(26)]
+        self.root = [None] * 26
         
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
-        index = ord(word[0]) - 65
-        self.letters[index] = Trie()
-        node = self.letters[index]
+        node = self.root[get_index(word[0])]
+        if not node:
+            self.root[get_index(word[0])] = TrieNode()
+        node = self.root[get_index(word[0])]
         for letter in word[1:]:
-            index = ord(letter) - 65
-            node.letters[index] = Trie()
-            node = node.letters[index]
+            pointer = node.letters[get_index(letter)]
+            if not pointer:
+                node.letters[get_index(letter)] = TrieNode()
+            node = node.letters[get_index(letter)]
         node.isEnd = True
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-        index = ord(letter) - 65
-        node = self.letters[index]
+        node = self.root[get_index(word[0])]
+        if not node:
+            return False
         for letter in word[1:]:
-            index = ord(letter) - 65
-            node = self.letters[index]
+            node = node.letters[get_index(letter)]
             if not node:
                 return False
-        if node.isEnd:
-            return True
-        return False
+        return node and node.isEnd
+        
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
+        node = self.root[get_index(prefix[0])]
+        if not node:
+            return False
+        for letter in prefix[1:]:
+            node = node.letters[get_index(letter)]
+            if not node:
+                return False
+        return bool(node)
         
-
+def get_index(letter: str) -> int:
+    return ord(letter) - ord('a')
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
